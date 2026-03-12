d514a01104c8be870e0514512295e5b4e7bf68cb
Raiyan Ahmed <raiyanahmed218@gmail.com>
2026-03-13 03:15:01 +1100

    started backend for BPT

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pathlib import Path
+from pydantic import BaseModel
+import json
+
+app = FastAPI()
+
+class Book(BaseModel):
+    title: str
+    total_pages: int
+    current_page: int
+
+
+@app.get("/")
+async def root():
+    return {"message": "BPT API is running"}
+
+# retrieve data
+@app.get("/books/{username}")
+async def get_books(username: str):
+    file = Path("Users") / f"{username}.json"
+    if not file.exists():
+        return {"error": "User not found"}
+    with open(file, 'r') as f:
+        return json.load(f)
+    
+
+@app.post("/books/{username}")
+async def add_book(username: str, book: Book):
+        file = Path("Users") / f"{username}.json" # this makes a Path object
+        with open(file, 'w') as f:
+                json.dump(book, f)
+        return {"Successfully added entry!"}
+    
+# TO DO NEXT: figure out how to read from file in main.py and error check like in bookPageTracker.py
