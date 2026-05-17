422a0374a517074ed611372ec59985d797edd317
Raiyan Ahmed <raiyanahmed218@gmail.com>
2026-05-18 02:42:24 +1000

    added status codes on exit

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ async def get_books(username: str):
     # because we dont open the file here, it wont create the file if it doesn't exist, so we can check for the file's existence before trying to read it
     file = get_user_file(username)
     if not file:
-        return {"error": "User not found"}
+        return fastapi.responses.JSONResponse(content={"error": "User not found"}, status_code=404)
     with open(file, 'r') as f:
-        return json.load(f)
+        return fastapi.responses.JSONResponse(content=json.load(f), status_code=200)
 
 # add a new user 
 @app.post("/newUser/{newUserName}")
@@ -42,22 +42,22 @@ async def make_new_user(newUserName: str):
     file.parent.mkdir(exist_ok=True, parents=True)
     # if the file already exists, return an error 
     if file.exists():
-        return {"error": f"User '{newUserName}' already exists"}
+        return fastapi.responses.JSONResponse(content={"error": f"User '{newUserName}' already exists"}, status_code=409)
     
     try:
         # this makes the file
         with open(file, 'w') as f:
             json.dump([], f)
-        return {"message": f"User '{newUserName}' successfully created"}
+        return fastapi.responses.JSONResponse(content={"message": f"User '{newUserName}' successfully created"}, status_code=201)
     except OSError as e:
-        return {"error": str(e)}
+        return fastapi.responses.JSONResponse(content={"error": str(e)}, status_code=400)
     
 # add a book to a user that exists
 @app.post("/books/{username}")
 async def add_book(username: str, book: Book):
         file = get_user_file(username)
         if not file:
-             return {"error": "User not found"}
+            return fastapi.responses.JSONResponse(content={"error": "User not found"}, status_code=404)
         with open(file, 'r') as f: # this will not create a new file if "file" doesnt exist, only in w or a mode it makes a new empty file if the file alr doesnt exist
             # 'r' - read, errors if file doesn't exist
             # 'w' - write, creates file if doesn't exist, OVERWRITES if it does
@@ -74,7 +74,7 @@ async def add_book(username: str, book: Book):
             
         for b in userData: # this is reading Book objects from a list
              if b["title"] == book.title:
-                  return {"error": "Book already exists"}
+                  return fastapi.responses.JSONResponse(content={"error": "Book already exists"}, status_code=409)
              
         # so bcz we got Field validators from Pydantic, we don't need to check for total_pages and current_page validity here
         # if book.total_pages <= 0:
@@ -87,14 +87,14 @@ async def add_book(username: str, book: Book):
 
         with open(file, 'w') as f: # we write here and not append because we read the existing data, modify and re-wrtie (overwriting the previous data).
                 json.dump(userData, f)
-        return {"message": "Successfully added entry!"}
+        return fastapi.responses.JSONResponse(content={"message": "Successfully added entry!"}, status_code=200)
 
 @app.put("/books/{username}/{prevBookName}") # we have prevBookName in the url because data can only be retrieved from body or from url and since we cant send two Book as python wont know which json structure to parse to prevBook and updatedBook, we changed prevBook to just the name and now we send it through url
 async def update_book(username: str, prevBookName: str, updatesToBook: Book):
     file = get_user_file(username)
     # check if file exists
     if not file:
-         return {"error": "User does not exist"}
+        return fastapi.responses.JSONResponse(content={"error": "User does not exist"}, status_code=404)
     
     # get the current userdata
     with open(file, 'r') as f:
@@ -114,16 +114,16 @@ async def update_book(username: str, prevBookName: str, updatesToBook: Book):
             # write new userdata back to file
             with open(file, 'w') as f:
                 json.dump(userData, f)
-            return {"message": f"Book '{prevBookName}' successfully updated"}
+            return fastapi.responses.JSONResponse(content={"message": f"Book '{prevBookName}' successfully updated"}, status_code=200)
     
-    return {"error": f"Book '{prevBookName}' not found"}
+    return fastapi.responses.JSONResponse(content={"error": f"Book '{prevBookName}' not found"}, status_code=404)
 
 @app.delete("/books/{username}/{bookName}")
 async def delete_book(username: str, bookName: str):
     file = get_user_file(username)
 
     if not file:
-        return {"error": "User does not exist"}
+        return fastapi.responses.JSONResponse(content={"error": "User does not exist"}, status_code=404)
 
     with open(file, "r") as f:
         userData = json.load(f)
@@ -135,7 +135,7 @@ async def delete_book(username: str, bookName: str):
             userData.remove(b)
             with open(file, "w") as f:
                 json.dump(userData, f)
-            return {"message": f"Book '{bookName}' successfully removed"}
+            return fastapi.responses.JSONResponse(content={"message": f"Book '{bookName}' successfully removed"}, status_code=200)
     
 # to run use -> python -m uvicorn main:app --reload
 
